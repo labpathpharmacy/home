43fd5f83f45b4c5897f467d626c9c0520c98bbf4
labpathpharmacy <labpath2026@gmail.com>
2026-07-01 14:57:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+       function toggleMenu() {
+            const menu = document.getElementById("ddM");
+            const btn = document.getElementById("mb");
+            menu.classList.toggle("show");
+            btn.classList.toggle("active");
+        }
+        function toggleMenu2() {
+            const menu2 = document.getElementById("ddM2");
+            const btn2 = document.getElementById("mb2");
+            menu2.classList.toggle("show");
+            btn2.classList.toggle("active");
+        }
